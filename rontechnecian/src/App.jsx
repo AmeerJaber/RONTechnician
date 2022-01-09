a191f8aab011229fdfec24904f6e2cad727d0dca
Ameer <ameerjaber97@gmail.com>
2022-01-09 13:46:54 +0200

    Added Navbar

--- rontechnecian/src/App.jsx
+++ rontechnecian/src/App.jsx
@@ -6,16 +6,24 @@ import About from'./About.jsx';
 import LogForm from'./LogForm.jsx';
 import { Login, Register } from "./components/login/index";
 import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import {Nav,Navbar,Container} from 'react-bootstrap';
 
 const App = () => {
   return (
     <Router>
       <div>
-        <Link to="/">Home </Link>
+      <Navbar bg="primary" variant="dark">
+      <Container>
+      <Navbar.Brand href="#" style={{marginRight: 80 }}>RonTechnician</Navbar.Brand>
+      <Nav className="me-auto">
+        <Link style={{ textDecoration: 'none',marginRight: 30, color: 'white' }} to="/">Home </Link>
+        <Link style={{ textDecoration: 'none',marginRight: 30, color: 'white' }} to="/about">About </Link>
 
-        <Link to="/about">About </Link>
+        <Link style={{ textDecoration: 'none',marginRight: 30, color: 'white' }} to="/logform">LogIn/Register </Link>
+        </Nav>
+        </Container>
+        </Navbar>
 
-        <Link to="/logform">LogIn/Register </Link>
       </div>
 
       <Switch>
